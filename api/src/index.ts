ee88000a484d7f920ce687292e0b8b522a4bc455
Philipp Moser <philipp.moser@finanzen.net>
2025-05-16 14:53:52 +0200

    PMO: Setup express entry

---
+++ api/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
