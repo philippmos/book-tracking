4c00072053a1444001068af3968df22ed208a52b
Philipp Moser <philipp@pmo-software.de>
2025-05-16 16:24:48 +0200

    PMO: Add to Rename

---
+++ api/src/controllers/BookController.ts
@@ -0,0 +1,27 @@
+import { Router, Request, Response } from 'express';
+import { IBookService } from '../services/interfaces/IBookService';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+export class BookController {
+    public readonly router: Router;
+    private readonly bookService: IBookService;
+
+    constructor(
+        @inject('IBookService')bookServiceInjection: IBookService
+    ) {
+        this.bookService = bookServiceInjection;
+        this.router = Router();
+        this.initRoutes();
+    }
+
+    private initRoutes(): void {
+        this.router.get('/', this.getAll);
+    }
+
+    private getAll = (_request: Request, response: Response): void => {
+        response
+            .status(200)
+            .json(this.bookService.getAll());
+    };
+}
